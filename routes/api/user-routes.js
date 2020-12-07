fb2850658d0e188b78b1f9c863d03ac4228e42e6
Nicole Gile <nicolegile94@gmail.com>
2020-12-07 12:43:25 -0600

    added hashing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -48,13 +48,36 @@ router.post('/', (req, res) => {
       });
   });
 
+// /api/users/login
+router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {  
     User.update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    })
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
       .then(dbUserData => {
         if (!dbUserData[0]) {
           res.status(404).json({ message: 'No user found with this id' });
