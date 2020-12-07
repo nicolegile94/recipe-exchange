fac1a74640caa894dd5d4ead3d29d9a60b585c0f
Nicole Gile <nicolegile94@gmail.com>
2020-12-07 13:35:41 -0600

    added recipe routes

---
+++ routes/api/recipe-routes.js
@@ -0,0 +1,105 @@
+const router = require('express').Router();
+const { Recipe, User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    Recipe.findAll({
+      attributes: ['id', 'recipe_url', 'title', 'created_at'],
+      order: [['created_at', 'DESC']],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbRecipeData => res.json(dbRecipeData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.get('/:id', (req, res) => {
+    Recipe.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'recipe_url', 'title', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbRecipeData => {
+        if (!dbRecipeData) {
+          res.status(404).json({ message: 'No recipe found with this id' });
+          return;
+        }
+        res.json(dbRecipeData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.post('/', (req, res) => {
+    Recipe.create({
+      title: req.body.title,
+      recipe_url: req.body.recipe_url,
+      user_id: req.body.user_id
+    })
+      .then(dbRecipeData => res.json(dbRecipeData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });  
+
+router.put('/:id', (req, res) => {
+    Recipe.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbRecipeData => {
+        if (!dbRecipeData) {
+          res.status(404).json({ message: 'No recipe found with this id' });
+          return;
+        }
+        res.json(dbRecipeData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.delete('/:id', (req, res) => {
+    Recipe.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbRecipeData => {
+        if (!dbRecipeData) {
+          res.status(404).json({ message: 'No recipe found with this id' });
+          return;
+        }
+        res.json(dbRecipeData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+module.exports = router;
