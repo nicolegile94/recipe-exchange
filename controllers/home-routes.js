68637be634a035a1ec0881275627a8140c0398d3
Nicole Gile <nicolegile94@gmail.com>
2020-12-07 18:48:57 -0600

    integrate homepage with handlebars

---
+++ controllers/home-routes.js
@@ -0,0 +1,45 @@
+const { constant } = require("lodash");
+const { route } = require("./api");
+
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Recipe, User, Comment } = require('../models');
+
+
+router.get('/', (req, res) => {
+    Recipe.findAll({
+      attributes: [
+        'id',
+        'recipe_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM madeit WHERE recipe.id = madeit.recipe_id)'), 'madeit_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'recipe_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbRecipeData => {
+        // pass a single post object into the homepage template
+        const recipes = dbRecipeData.map(recipe => recipe.get({ plain: true }));
+
+        res.render('homepage', { recipes });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const { route } = require('./api');
 const apiRoutes = require('./api');
 
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
