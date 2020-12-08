938e9dfd87a0d1a2d29147aac71dd25b83486b7b
Nicole Gile <nicolegile94@gmail.com>
2020-12-07 20:43:34 -0600

    added single recipt functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,7 +34,7 @@ router.get('/', (req, res) => {
         // pass a single post object into the homepage template
         const recipes = dbRecipeData.map(recipe => recipe.get({ plain: true }));
 
-        res.render('homepage', { recipes });
+        res.render('homepage', { recipes, loggedIn: req.session.loggedIn });
       })
       .catch(err => {
         console.log(err);
@@ -49,5 +49,49 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/recipe/:id', (req, res) => {
+  Recipe.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'recipe_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM madeit WHERE recipe.id = madeit.recipe_id)'), 'madeit_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'recipe_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbRecipeData => {
+      if (!dbRecipeData) {
+        res.status(404).json({ message: 'No recipe found with this id' });
+        return;
+      }
+
+      // serialize the data
+      const recipe = dbRecipeData.get({ plain: true });
+
+      // pass data to template
+      res.render('single-recipe', { recipe, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
